ddb8a42d1f3ce86551c3b6cac8fc2a9a6beee8c5
Tharindu Hewawitharana <thasanhe1990@gmail.com>
2017-01-25 03:28:13 +0530

    Added article add feature

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -10,11 +10,7 @@ import { Article } from './article.model';
   }
 })
 export class ArticleComponent implements OnInit {
-  article: Article;
-
-  constructor() {
-    this.article = new Article('Angular 2', 'http://angular.io', 10);
-  }
+  @Input() article: Article;
 
   voteUp(): boolean {
     this.article.voteUp();
